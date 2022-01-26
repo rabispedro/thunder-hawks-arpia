05709f6b2bd1eaec8a99c4e774e298658aea242b
Pedro Rabis <rabis.pedro@gmail.com>
2022-01-26 19:43:42 -0300

    :sparkles: Query iniciais finalizadas

---
+++ Sprint_02/Atividade 7/query.sql
@@ -0,0 +1,36 @@
+-- 1 Retornar todos alunos ordenados por turmas , por nome em ordem alfabetica
+
+SELECT
+		tb_aluno.id_turma,
+		tb_aluno.nome
+FROM
+		tb_aluno,
+		tb_turma
+ORDER BY
+		tb_aluno.id_turma,
+		tb_aluno.nome
+		ASC;
+
+-- 2) Retornar as turmas em que não existem alunos
+SELECT
+		tb_turma.nome
+FROM
+		tb_aluno,
+		tb_turma
+WHERE
+	tb_aluno.id_turma is NULL;
+
+-- 3) Retornar as especialidades dos professores que possuem turmas
+
+SELECT
+	tb_especialidade.nome
+FROM
+	tb_especialidade,
+	tb_professor,
+	tb_turma
+WHERE
+	tb_turma.id_professor is not NULL AND
+	tb_turma.id_professor = tb_professor.id AND
+	tb_professor.id_especialidade = tb_especialidade.id;
+
+
