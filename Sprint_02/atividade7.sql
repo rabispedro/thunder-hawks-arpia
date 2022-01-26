ee4f9df9ecb18b5e104184d2cd67ae43562d28a8
Pedro Rabis <rabis.pedro@gmail.com>
2022-01-26 13:52:23 -0300

    :zap: Banco de dados melhorado

--- Sprint_02/atividade7.sql
+++ Sprint_02/atividade7.sql
@@ -1,34 +1,40 @@
 CREATE TABLE tb_especialidade (
-    id UUID UNIQUE NOT NULL PRIMARY KEY,
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
     nome TEXT NOT NULL
 );
 
 CREATE TABLE tb_professor (
-    id UUID NOT NULL PRIMARY KEY,
-    id_especialidade UUID REFERENCES tb_especialidade(id),
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    id_especialidade INT REFERENCES tb_especialidade(id),
     nome TEXT NOT NULL,
     data_nascimento TIMESTAMP NOT NULL
 );
 
 CREATE TABLE tb_turma (
-    id UUID UNIQUE NOT NULL PRIMARY KEY,
-    id_professor UUID REFERENCES tb_professor(id),
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    id_professor INT REFERENCES tb_professor(id),
     nome TEXT NOT NULL,
     turno TEXT NOT NULL,
     serie TEXT NOT NULL
 );
 
 CREATE TABLE tb_aluno (
-	id UUID NOT NULL PRIMARY KEY,
-    id_turma UUID REFERENCES tb_turma(id),
+    id SERIAL UNIQUE NOT NULL PRIMARY KEY,
+    id_turma INT REFERENCES tb_turma(id),
     nome TEXT NOT NULL,
-    matricula TEXT NOT NULL,
+    matricula UUID UNIQUE NOT NULL,
     nome_pai TEXT,
     nome_mae TEXT,
     sexo TEXT,
     data_nascimento TIMESTAMP NOT NULL,
-    data_matricula TIMESTAMP NOT NULL  	
+    data_matricula TIMESTAMP NOT NULL      
 );
 
-INSERT INTO tb_especialidade(id, nome) VALUES(gen_random_uuid(), 'Matemática')
-
+INSERT INTO tb_especialidade(nome) VALUES('Matemática');
+INSERT INTO tb_especialidade(nome) VALUES('História');
+INSERT INTO tb_especialidade(nome) VALUES('Geografia');
+INSERT INTO tb_especialidade(nome) VALUES('Português');
+INSERT INTO tb_especialidade(nome) VALUES('Química');
+INSERT INTO tb_especialidade(nome) VALUES('Física');
+INSERT INTO tb_especialidade(nome) VALUES('Redação');
+INSERT INTO tb_especialidade(nome) VALUES('Educação Física');
